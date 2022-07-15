dfe2a1eaf9ca8b69f8f705dfd329c98ff881e06c
Boccarusso <gabrieleboccarusso@gmail.com>
2022-07-15 09:32:17 +0200

    added possibility to get posts by a tag

--- src/main/java/boccarusso/controller/PostController.java
+++ src/main/java/boccarusso/controller/PostController.java
@@ -3,14 +3,13 @@ package boccarusso.controller;
 import boccarusso.repository.PostRepository;
 import boccarusso.table.Post;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.LinkedList;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class PostController {
@@ -36,5 +35,17 @@ public class PostController {
 	@GetMapping("/posts/between/{end}/{begin}")
 	Iterable<Post> postsBetween(@PathVariable int end, @PathVariable int begin) {
 		return repository.findByIdBetweenOrderByIdDesc(end, begin);
+	}
+	
+	@CrossOrigin(origins = "*")
+	@GetMapping("/posts/getByTag/{tag}")
+	LinkedList<Optional<Post>> name(@PathVariable String tag){
+		LinkedList<Optional<Post>> posts = new LinkedList<>();
+		Iterable<Integer> ids = repository.getIdsWithTag(tag);
+		for(Integer id : ids) {
+			Optional<Post> p = repository.findById(id);
+			posts.add(p);
+		}
+		return posts;
 	}
 }
--- src/main/java/boccarusso/repository/PostRepository.java
+++ src/main/java/boccarusso/repository/PostRepository.java
@@ -3,11 +3,16 @@ package boccarusso.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import boccarusso.table.AppliedTag;
 import boccarusso.table.Post;
+import boccarusso.table.Tag;
 
 public interface PostRepository extends JpaRepository<Post, Integer>{
 	Iterable<Post> findByIdBetweenOrderByIdDesc(int end, int begin);
 	
 	@Query("SELECT max(id) FROM Post")
 	public Long findMaxId();
+	
+	@Query("SELECT ap.postId FROM AppliedTag ap WHERE ap.tagId = (SELECT t.id FROM Tag t WHERE t.name = :name)")
+	Iterable<Integer> getIdsWithTag (String name);
 }
