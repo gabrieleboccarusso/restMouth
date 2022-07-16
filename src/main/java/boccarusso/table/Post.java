db8c6804b654f0cfad1ddb763dda2a8b255056f3
Boccarusso <gabrieleboccarusso@gmail.com>
2022-07-16 12:15:42 +0200

    made content field from varchar to TEXT

--- src/main/java/boccarusso/table/Post.java
+++ src/main/java/boccarusso/table/Post.java
@@ -1,5 +1,6 @@
 package boccarusso.table;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Post {
 	private String tags;
 	private String creation;
 	private String lastUpdate;
+	@Column(columnDefinition="TEXT")
 	private String content;
 	
 	public Integer getId() {
